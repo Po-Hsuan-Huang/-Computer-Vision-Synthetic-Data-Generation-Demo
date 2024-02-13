a6799b94ac86f273e2d14271a5aee4baa3fb0438
po-hsuan-huang <lets.lincredible@gmail.com>
2024-02-13 00:30:36 -0800

    wrong folder

--- marathon/sync.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# open -u <user> <password> <host url>; mirror -c -R -L <path from> <path to>
-lftp -c "open -u pohsuan.huang,acer 10.36.169.170; mirror -c -R -L /home/pohsuan/disk1/Marathon/JPEGImages/TrainSet8/ /home/pohsuan.huang/pva-faster-rcnn/data/VOCdevkit2007/sycfolder/JPEGImages"
-
-lftp -c "open -u pohsuan.huang,acer 10.36.169.170; mirror -c -R -L /home/pohsuan/disk1/Marathon/Annotations/TrainSet8/ /home/pohsuan.huang/pva-faster-rcnn/data/VOCdevkit2007/sycfolder/Annotations"
